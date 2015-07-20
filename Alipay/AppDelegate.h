21e4ae5d984bf02d88e892c6650f12e8ad5d277a
jobsyu <1175585627@qq.com>
2015-07-20 14:18:19 +0800

    Initial Commit

---
+++ Alipay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alipay
+//
+//  Created by jobs on 15/7/20.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
